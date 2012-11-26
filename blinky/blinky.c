bb5629b7f431d386017396ab10e14b47af057712
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2012-11-26 13:17:58 +0100

    Make blink the led on pin 13 of a standard Arduino.

--- blinky/blinky.c
+++ blinky/blinky.c
@@ -6,22 +6,27 @@
 #include <util/delay.h>
 
 // http://balau82.wordpress.com/2011/03/29/programming-arduino-uno-in-pure-c/
+/*
+ * My advice is to test that actually this code result in a
+ * blinking led with delay of 1 second. Otherwise your F_CPU
+ * setting is wrong.
+ */
 enum {
- BLINK_DELAY_MS = 100,
+ BLINK_DELAY_MS = 1000,
 };
 
 
 int main (void) {
  /* set pin 5 of PORTB for output*/
- DDRB |= _BV(DDB1);
+ DDRB |= _BV(DDB5);
 
  while(1) {
   /* set pin 5 high to turn led on */
-  PORTB |= _BV(PORTB1);
+  PORTB |= _BV(PORTB5);
   _delay_ms(BLINK_DELAY_MS);
 
   /* set pin 5 low to turn led off */
-  PORTB &= ~_BV(PORTB1);
+  PORTB &= ~_BV(PORTB5);
   _delay_ms(BLINK_DELAY_MS);
  }
 
