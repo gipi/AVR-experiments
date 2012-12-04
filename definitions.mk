581fc3976acd2a5ead4e639d61ff5d472d685981
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2012-12-04 15:48:38 +0100

    Refactor makefiles.
    There are two sub makefiles, one with the default value of various
    variables and one with the predefined targets. In the projects' Makefile
    we must define at least OBJ_FILES and TARGET variables.

---
+++ definitions.mk
@@ -0,0 +1,16 @@
+
+MCU          = atmega328p
+
+F_CPU        = 1000000
+ARDUINO_PORT = /dev/ttyUSB0
+
+AVRDUDE_ARD_PROGRAMMER = buspirate
+AVRDUDE_ARD_BAUDRATE   = 115200
+AVRDUDE_ARD_PORT       = /dev/ttyUSB0
+AVRDUDE_OPTS           = -c $(AVRDUDE_ARD_PROGRAMMER) -p $(MCU) -P $(AVRDUDE_ARD_PORT) -b $(AVRDUDE_ARD_BAUDRATE)
+
+CC         = avr-gcc
+OBJCOPY    = avr-objcopy
+DUDE       = avrdude
+CPPFLAGS   = -Os -DF_CPU=$(F_CPU) -mmcu=$(MCU) -Wall
+
