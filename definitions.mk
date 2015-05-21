0ccd505f987eab8840b7f94542ffec9e834a9733
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2015-05-21 17:55:15 +0200

    Make possible to upload code to Arduino.

--- definitions.mk
+++ definitions.mk
@@ -4,9 +4,27 @@ TOP := $(dir $(lastword $(MAKEFILE_LIST)))
 
 LIBRARY_INCLUDE = $(TOP)libraries/
 
+ifndef PROGRAMMER
+
 AVRDUDE_ARD_PROGRAMMER = buspirate
 AVRDUDE_ARD_BAUDRATE   = 115200
 AVRDUDE_ARD_PORT       = /dev/ttyUSB0
+
+else
+
+ifeq ($(PROGRAMMER), arduino)
+
+AVRDUDE_ARD_PROGRAMMER = arduino
+AVRDUDE_ARD_BAUDRATE   = 115200
+AVRDUDE_ARD_PORT       = /dev/ttyACM0
+
+MCU          = atmega328p
+F_CPU        = 16000000UL
+
+
+endif
+
+endif
 AVRDUDE_OPTS           = -c $(AVRDUDE_ARD_PROGRAMMER) -p $(MCU) -P $(AVRDUDE_ARD_PORT) -b $(AVRDUDE_ARD_BAUDRATE)
 
 CC         = avr-gcc
