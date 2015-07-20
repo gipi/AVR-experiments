8fa01e8b4733b8b643c485ecdfba19a6d4eb534e
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2015-07-20 15:47:40 +0200

    Add usb type programmer.

--- definitions.mk
+++ definitions.mk
@@ -10,21 +10,24 @@ AVRDUDE_ARD_PROGRAMMER := buspirate
 AVRDUDE_ARD_BAUDRATE   = 115200
 AVRDUDE_ARD_PORT       = /dev/ttyUSB0
 
-else
-
-ifeq ($(PROGRAMMER), arduino)
+else ifeq ($(PROGRAMMER), arduino)
 
 AVRDUDE_ARD_PROGRAMMER = arduino
 AVRDUDE_ARD_BAUDRATE   = 115200
 AVRDUDE_ARD_PORT       = /dev/ttyACM0
 
+else ifeq ($(PROGRAMMER),usb)
+
+AVRDUDE_ARD_PROGRAMMER = avr109
+AVRDUDE_ARD_BAUDRATE   = 115200
+AVRDUDE_ARD_PORT       = /dev/ttyACM0
+
+endif
+
 MCU          = atmega328p
 F_CPU        = 16000000UL
 
 
-endif
-
-endif
 AVRDUDE_OPTS           = -c $(AVRDUDE_ARD_PROGRAMMER) -p $(MCU) -P $(AVRDUDE_ARD_PORT) -b $(AVRDUDE_ARD_BAUDRATE)
 
 CC         = avr-gcc
